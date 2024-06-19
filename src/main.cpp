e564b72795bd6002cf3b9f0648b58674245ab08b
solvershade <seconduacc07@gmail.com>
2024-06-19 12:02:51 -0400

    window now renders.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
-#include "test/t.h"
 #include <GLFW/glfw3.h>
 #include <genie/GenieView.hpp>
 #include <iostream>
 
+void initWindow() {
+  std::cout << "bam" << std::endl;
+
+  if (!glfwInit()) {
+    std::cout << "Failed to initialize GLFW" << std::endl;
+  }
+
+  GenieView genieView = GenieView(640, 480, "myWindow");
+  GLFWwindow *window = genieView.Window;
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    // Clear the screen
+    // glClear(GL_COLOR_BUFFER_BIT);
+
+    // Swap front and back buffers
+    glfwSwapBuffers(window);
+
+    // Poll for and process events
+    glfwPollEvents();
+  }
+}
+
 int main() {
   std::cout << "Hello, Uncle Leo!" << std::endl;
 
-  // GenieView genieView = GenieView(640, 480, "my_window");
-  T t = T();
-  t.sayHello();
+  initWindow();
 
   return 0;
 }
